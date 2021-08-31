467d34f5296353cfe029f7936e3de1e9e6969c81
kokokaki <hsg9984@gmail.com>
2021-08-31 15:56:11 +0900

    [21.08.31] # 서브쿼리 학습

--- 예제/9. 서브쿼리.sql
+++ 예제/9. 서브쿼리.sql
@@ -66,6 +66,83 @@ ORDER BY A.emp_no
 ;
 
 
+-- # 다중 컬럼 서브쿼리
+
+-- 부서원이 2명 이상인 부서 중에서 각 부서의 가장 연장자의 사원정보를 조회
+
+SELECT 
+    A.emp_no, A.emp_nm, A.birth_de
+    , A.dept_cd, B.dept_nm
+FROM tb_emp A
+JOIN tb_dept B
+ON A.dept_cd = B.dept_cd
+WHERE (A.dept_cd, A.birth_de) IN (
+                                        SELECT 
+                                            dept_cd, MIN(birth_de)
+                                        FROM tb_emp
+                                        GROUP BY dept_cd
+                                        HAVING COUNT(*) >= 2
+                                    )
+ORDER BY A.emp_no
+;
+
+
+
+-- EXISTS 문
+
+-- 주소가 강남인 직원들이 근무하고 있는 부서들의 부서정보를 조회
+
+SELECT 
+    emp_nm, addr, dept_cd
+FROM tb_emp
+WHERE addr LIKE '%강남%'
+;
+
+SELECT * FROM tb_dept WHERE dept_cd IN ('100009', '100010');
+
+
+SELECT 1 FROM tb_emp WHERE addr LIKE '%서울%';
+
+
+
+SELECT 
+    A.dept_cd , A.dept_nm
+FROM tb_dept A
+WHERE EXISTS (
+                                SELECT 
+                                    'a'
+                                FROM tb_emp B
+                                WHERE B.addr LIKE '%강남%'
+                                        AND A.dept_cd = B.dept_cd
+                            )
+;
+
+
+-- 스칼라 서브쿼리 (SELECT절에 쓰는 서브쿼리)
+SELECT 
+    A.emp_no, A.emp_nm
+    , (SELECT dept_nm FROM tb_dept WHERE A.dept_cd = B.dept_cd) AS dept_nm
+    , A.addr, A.birth_de
+FROM tb_emp A
+;
+
+
+-- 인라인 뷰 (FROM절에 쓰는 서브쿼리 )
+
+
+SELECT 
+    B.emp_no, B.emp_nm , A.pay_avg
+FROM (
+                SELECT emp_no ,AVG(pay_amt) AS pay_avg
+                FROM tb_sal_his 
+                GROUP BY emp_no
+            ) A, tb_emp B
+WHERE A.emp_no = B.emp_no
+;
+
+
+
+
 
 
 
